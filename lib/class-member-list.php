e9ec0e0927258f07a85aeaec78157766a1833a9d
Ralph John Galindo <ralphjesy@gmail.com>
2016-06-28 12:27:48 +0800

    Done adding to mailing list
    * Ajax Support
    * Frontend Shortcode
    * Subscriber Info

--- lib/class-member-list.php
+++ lib/class-member-list.php
@@ -11,46 +11,46 @@ class Members_List extends WP_List_Table {
 			'singular' => __( 'Customer', 'sp' ), //singular name of the listed records
 			'plural'   => __( 'Customers', 'sp' ), //plural name of the listed records
 			'ajax'     => false //does this table support ajax?
-		] );
-	}
-	/**
-	 * Retrieve customers data from the database
-	 *
-	 * @param int $per_page
-	 * @param int $page_number
-	 *
-	 * @return mixed
-	 */
-	public static function get_customers( $per_page = 5, $page_number = 1 ) {
-		global $wpdb;
-		$sql = "SELECT * FROM {$wpdb->prefix}subscribers";
-		if ( ! empty( $_REQUEST['orderby'] ) ) {
-			$sql .= ' ORDER BY ' . esc_sql( $_REQUEST['orderby'] );
-			$sql .= ! empty( $_REQUEST['order'] ) ? ' ' . esc_sql( $_REQUEST['order'] ) : ' ASC';
+			] );
 		}
-		$sql .= " LIMIT $per_page";
-		$sql .= ' OFFSET ' . ( $page_number - 1 ) * $per_page;
-		$result = $wpdb->get_results( $sql, 'ARRAY_A' );
-		return $result;
-	}
-	/**
-	 * Delete a customer record.
-	 *
-	 * @param int $id customer ID
-	 */
-	public static function delete_customer( $id ) {
-		global $wpdb;
-		$wpdb->delete(
+		/**
+		* Retrieve customers data from the database
+		*
+		* @param int $per_page
+		* @param int $page_number
+		*
+		* @return mixed
+		*/
+		public static function get_customers( $per_page = 5, $page_number = 1 ) {
+			global $wpdb;
+			$sql = "SELECT * FROM {$wpdb->prefix}subscribers";
+			if ( ! empty( $_REQUEST['orderby'] ) ) {
+				$sql .= ' ORDER BY ' . esc_sql( $_REQUEST['orderby'] );
+				$sql .= ! empty( $_REQUEST['order'] ) ? ' ' . esc_sql( $_REQUEST['order'] ) : ' ASC';
+			}
+			$sql .= " LIMIT $per_page";
+			$sql .= ' OFFSET ' . ( $page_number - 1 ) * $per_page;
+			$result = $wpdb->get_results( $sql, 'ARRAY_A' );
+			return $result;
+		}
+		/**
+		* Delete a customer record.
+		*
+		* @param int $id customer ID
+		*/
+		public static function delete_customer( $id ) {
+			global $wpdb;
+			$wpdb->delete(
 			"{$wpdb->prefix}subscribers",
-			[ 'ID' => $id ],
+			[ 'id' => $id ],
 			[ '%d' ]
 		);
 	}
 	/**
-	 * Returns the count of records in the database.
-	 *
-	 * @return null|string
-	 */
+	* Returns the count of records in the database.
+	*
+	* @return null|string
+	*/
 	public static function record_count() {
 		global $wpdb;
 		$sql = "SELECT COUNT(*) FROM {$wpdb->prefix}subscribers";
@@ -61,103 +61,105 @@ class Members_List extends WP_List_Table {
 		_e( 'No subscribers yet.', 'sp' );
 	}
 	/**
-	 * Render a column when no column specific method exist.
-	 *
-	 * @param array $item
-	 * @param string $column_name
-	 *
-	 * @return mixed
-	 */
+	* Render a column when no column specific method exist.
+	*
+	* @param array $item
+	* @param string $column_name
+	*
+	* @return mixed
+	*/
 	public function column_default( $item, $column_name ) {
 		switch ( $column_name ) {
-			case 'address':
-			case 'city':
-				return $item[ $column_name ];
+			case 'email':
+			case 'fullname':
+			case 'displayname':
+			case 'contact':
+			return !empty($item[ $column_name ]) ? trim($item[ $column_name ]) : '-';
+			case 'status':
+			return !empty($item[ $column_name ]) ? ['Blocked','Verified','Pending'][$item[ $column_name ]] : 'N/A';
 			default:
-				return print_r( $item, true ); //Show the whole array for troubleshooting purposes
+			return print_r( $item, true ); //Show the whole array for troubleshooting purposes
 		}
 	}
 	/**
-	 * Render the bulk edit checkbox
-	 *
-	 * @param array $item
-	 *
-	 * @return string
-	 */
+	* Render the bulk edit checkbox
+	*
+	* @param array $item
+	*
+	* @return string
+	*/
 	function column_cb( $item ) {
 		return sprintf(
-			'<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['ID']
-		);
-	}
-	/**
-	 * Method for name column
-	 *
-	 * @param array $item an array of DB data
-	 *
-	 * @return string
-	 */
-	function column_name( $item ) {
-		$delete_nonce = wp_create_nonce( 'sp_delete_customer' );
-		$title = '<strong>' . $item['name'] . '</strong>';
-		$actions = [
-			'delete' => sprintf( '<a href="?page=%s&action=%s&customer=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['ID'] ), $delete_nonce )
-		];
-		return $title . $this->row_actions( $actions );
-	}
-	/**
-	 *  Associative array of columns
-	 *
-	 * @return array
-	 */
-	function get_columns() {
-		$columns = [
-			'cb'      => '<input type="checkbox" />',
-			'email'    => __( 'Email Address', 'sp' ),
-			'firstname'    => __( 'First Name', 'sp' ),
-			'middlename' => __( 'Middle Name', 'sp' ),
-			'lastname' => __( 'Last Name', 'sp' ),
-			'displayname' => __( 'Nickname', 'sp' ),
-			'contact' => __( 'Contact', 'sp' ),
-		];
-		return $columns;
-	}
-	/**
-	 * Columns to make sortable.
-	 *
-	 * @return array
-	 */
-	public function get_sortable_columns() {
-		$sortable_columns = array(
-			'email' => array( 'email', true ),
-			'firstname' => array( 'firstname', false ),
-			'lastname' => array( 'lastname', false )
-		);
-		return $sortable_columns;
-	}
-	/**
-	 * Returns an associative array containing the bulk action
-	 *
-	 * @return array
-	 */
-	public function get_bulk_actions() {
-		$actions = [
-			'bulk-delete' => 'Delete'
-		];
-		return $actions;
-	}
-	/**
-	 * Handles data query and filter, sorting, and pagination.
-	 */
-	public function prepare_items() {
-		$this->_column_headers = $this->get_column_info();
-		/** Process bulk action */
-		$this->process_bulk_action();
-		$per_page     = $this->get_items_per_page( 'customers_per_page', 5 );
-		$current_page = $this->get_pagenum();
-		$total_items  = self::record_count();
-		$this->set_pagination_args( [
-			'total_items' => $total_items, //WE have to calculate the total number of items
-			'per_page'    => $per_page //WE have to determine how many items to show on a page
+		'<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['id']
+	);
+}
+/**
+* Method for name column
+*
+* @param array $item an array of DB data
+*
+* @return string
+*/
+function column_name( $item ) {
+	$delete_nonce = wp_create_nonce( 'sp_delete_customer' );
+	$title = '<strong>' . $item['name'] . '</strong>';
+	$actions = [
+		'delete' => sprintf( '<a href="?page=%s&action=%s&customer=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['id'] ), $delete_nonce )
+	];
+	return $title . $this->row_actions( $actions );
+}
+/**
+*  Associative array of columns
+*
+* @return array
+*/
+function get_columns() {
+	$columns = [
+		'cb'      => '<input type="checkbox" />',
+		'email'    => __( 'Email Address', 'sp' ),
+		'fullname'    => __( 'Full Name', 'sp' ),
+		'displayname' => __( 'Nickname', 'sp' ),
+		'contact' => __( 'Contact', 'sp' ),
+		'status' => __( 'Status', 'sp' ),
+	];
+	return $columns;
+}
+/**
+* Columns to make sortable.
+*
+* @return array
+*/
+public function get_sortable_columns() {
+	$sortable_columns = array(
+		'email' => array( 'email', true ),
+		'fullname' => array( 'fullname', false ),
+	);
+	return $sortable_columns;
+}
+/**
+* Returns an associative array containing the bulk action
+*
+* @return array
+*/
+public function get_bulk_actions() {
+	$actions = [
+		'bulk-delete' => 'Delete'
+	];
+	return $actions;
+}
+/**
+* Handles data query and filter, sorting, and pagination.
+*/
+public function prepare_items() {
+	$this->_column_headers = $this->get_column_info();
+	/** Process bulk action */
+	$this->process_bulk_action();
+	$per_page     = $this->get_items_per_page( 'customers_per_page', 5 );
+	$current_page = $this->get_pagenum();
+	$total_items  = self::record_count();
+	$this->set_pagination_args( [
+		'total_items' => $total_items, //WE have to calculate the total number of items
+		'per_page'    => $per_page //WE have to determine how many items to show on a page
 		] );
 		$this->items = self::get_customers( $per_page, $current_page );
 	}
@@ -171,15 +173,15 @@ class Members_List extends WP_List_Table {
 			}
 			else {
 				self::delete_customer( absint( $_GET['customer'] ) );
-		                // esc_url_raw() is used to prevent converting ampersand in url to "#038;"
-		                // add_query_arg() return the current url
-		                wp_redirect( esc_url_raw(add_query_arg()) );
+				// esc_url_raw() is used to prevent converting ampersand in url to "#038;"
+				// add_query_arg() return the current url
+				wp_redirect( esc_url_raw(add_query_arg()) );
 				exit;
 			}
 		}
 		// If the delete bulk action is triggered
 		if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-delete' )
-		     || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-delete' )
+		|| ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-delete' )
 		) {
 			$delete_ids = esc_sql( $_POST['bulk-delete'] );
 			// loop over the array of record IDs and delete them
@@ -187,8 +189,8 @@ class Members_List extends WP_List_Table {
 				self::delete_customer( $id );
 			}
 			// esc_url_raw() is used to prevent converting ampersand in url to "#038;"
-		        // add_query_arg() return the current url
-		        wp_redirect( esc_url_raw(add_query_arg()) );
+			// add_query_arg() return the current url
+			wp_redirect( esc_url_raw(add_query_arg()) );
 			exit;
 		}
 	}
--- lib/class-subscribe-now.php
+++ lib/class-subscribe-now.php
@@ -1,170 +1,221 @@
 <?php
 if(!class_exists('SubscribeNow'))
 {
-  class SubscribeNow
-  {
-    static $instance;
-    public $customers;
-    private $tablename = 'subscribers';
-    /**
-    * Construct the plugin object
-    */
-    public function __construct()
+    class SubscribeNow
     {
+        static $instance;
+        public $customers;
+        private $tablename = 'subscribers';
+        /**
+        * Construct the plugin object
+        */
+        public function __construct()
+        {
 
 
-      add_action('admin_init', array(&$this, 'admin_init'));
-      add_action('admin_menu', array(&$this, 'add_menu'));
+            add_action('admin_init', array(&$this, 'admin_init'));
+            add_action('admin_menu', array(&$this, 'add_menu'));
 
-      add_filter( 'set-screen-option', [ __CLASS__, 'set_screen' ], 10, 3 );
+            add_filter( 'set-screen-option', [ __CLASS__, 'set_screen' ], 10, 3 );
 
-      add_action('init','ajax_subscribe_init');
+            add_action('init','ajax_subscribe_init');
 
-    } // END public function __construct
+        } // END public function __construct
 
 
-    public function admin_init()
-    {
-      // Set up the settings for this plugin
-      $this->init_settings();
-      // Possibly do additional admin_init tasks
+        public function admin_init()
+        {
+            // Set up the settings for this plugin
+            $this->init_settings();
+            // Possibly do additional admin_init tasks
+        } // END public static function activate
+
+        /**
+        * Initialize some custom settings
+        */
+        public function init_settings()
+        {
+            // register the settings for this plugin
+            register_setting('wp_plugin_template-group', 'setting_a');
+            register_setting('wp_plugin_template-group', 'setting_b');
+        } // END public function init_custom_settings()
+
+        /**
+        * add a menu
+        */
+        public function add_menu()
+        {
+
+            // add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);
+
+            $hook = add_options_page(
+            'Subscribe Now',
+            'Subscribe Now',
+            'manage_options',
+            'wp_plugin_template',
+            array(&$this, 'plugin_settings_page'));
+
+            add_action( "load-$hook", array(&$this ,'plugin_settings_option') );
+        } // END public function add_menu()
+
+        /**
+        * Menu Callback
+        */
+        public function plugin_settings_page()
+        {
+
+            if(!current_user_can('manage_options'))
+            {
+                wp_die(__('You do not have sufficient permissions to access this page.'));
+            }
+
+            // Render the settings template
+            include(sprintf("%s/templates/settings.php", SUBSCRIBE_NOW_PLUGIN_DIR));
+        } // END public function plugin_settings_page()
+
+        /**
+        * Menu Options Callback
+        */
+        public function plugin_settings_option()
+        {
+            $option = 'per_page';
+            $args   = [
+                'label'   => 'Subscribers per page',
+                'default' => 5,
+                'option'  => 'customers_per_page'
+            ];
+
+            add_screen_option( $option, $args );
+
+            $this->customers = new Members_List();
+        }
+
+        public static function set_screen( $status, $option, $value ) {
+            return $value;
+        }
+        /**
+        * Attached to activate_{ plugin_basename( __FILES__ ) } by register_activation_hook()
+        * @static
+        */
+
+        public static function activate()
+        {
+
+            global $wpdb;
+
+            $table = $wpdb->prefix . 'subscribers';
+
+            // create the ECPT metabox database table
+            if($wpdb->get_var("show tables like '$table'") != $table)
+            {
+                $sql = "CREATE TABLE " . $table . " (
+                `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+                `fullname` TINYTEXT,
+                `displayname` VARCHAR(255),
+                `contact` VARCHAR(255),
+                `email` VARCHAR(100) NOT NULL,
+                `activation_key` VARCHAR(255) NOT NULL,
+                `status` TINYINT(1) NOT NULL,
+                UNIQUE KEY id (id)
+            );";
+
+
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            dbDelta($sql);
+        }
+
+        if ( version_compare( $GLOBALS['wp_version'], SUBSCRIBE_NOW_MIN_WP_VERSION, '<' ) ) {
+            load_plugin_textdomain( 'subscribenow' );
+
+            $message = '<strong>'.sprintf( 'Subscribe now %s requires WordPress %s or higher.', SUBSCRIBE_NOW_VERSION, SUBSCRIBE_NOW_MIN_WP_VERSION ).'</strong> '.sprintf('Please <a href="%1$s">upgrade WordPress</a> to a current version.', 'https://codex.wordpress.org/Upgrading_WordPress', 'https://wordpress.org/extend/plugins/akismet/download/');
+
+            SubsribeNow::bail_on_activation( $message );
+        }
     } // END public static function activate
 
-    /**
-    * Initialize some custom settings
-    */
-    public function init_settings()
+
+    public static function deactivate()
     {
-      // register the settings for this plugin
-      register_setting('wp_plugin_template-group', 'setting_a');
-      register_setting('wp_plugin_template-group', 'setting_b');
-    } // END public function init_custom_settings()
-
-    /**
-    * add a menu
-    */
-    public function add_menu()
-    {
-
-      // add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);
-
-      $hook = add_options_page(
-      'Subscribe Now',
-      'Subscribe Now',
-      'manage_options',
-      'wp_plugin_template',
-      array(&$this, 'plugin_settings_page'));
-
-      add_action( "load-$hook", array(&$this ,'plugin_settings_option') );
-    } // END public function add_menu()
-
-    /**
-    * Menu Callback
-    */
-    public function plugin_settings_page()
-    {
-
-      if(!current_user_can('manage_options'))
-      {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
-      }
-
-      // Render the settings template
-      include(sprintf("%s/templates/settings.php", SUBSCRIBE_NOW_PLUGIN_DIR));
-    } // END public function plugin_settings_page()
-
-    /**
-    * Menu Options Callback
-    */
-    public function plugin_settings_option()
-    {
-      $option = 'per_page';
-      $args   = [
-        'label'   => 'Subscribers per page',
-        'default' => 5,
-        'option'  => 'customers_per_page'
-      ];
-
-      add_screen_option( $option, $args );
-
-      $this->customers = new Members_List();
-    }
-
-    public static function set_screen( $status, $option, $value ) {
-      return $value;
-    }
-    /**
-    * Attached to activate_{ plugin_basename( __FILES__ ) } by register_activation_hook()
-    * @static
-    */
-
-    public static function activate()
-    {
-
-      global $wpdb;
-
-      $table = $wpdb->prefix . 'subscribers';
-
-      // create the ECPT metabox database table
-      if($wpdb->get_var("show tables like '$table'") != $table)
-      {
-        $sql = "CREATE TABLE " . $table . " (
-        `id` mediumint(9) NOT NULL AUTO_INCREMENT,
-        `firstname` TINYTEXT,
-        `middlename` TINYTEXT,
-        `lastname` TINYTEXT,
-        `displayname` VARCHAR(255),
-        `contact` VARCHAR(255),
-        `email` VARCHAR(100) NOT NULL,
-        `activation_key` VARCHAR(255) NOT NULL,
-        `status` TINYINT(1) NOT NULL,
-        UNIQUE KEY id (id)
-      );";
-
-
-      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-      dbDelta($sql);
-    }
-
-    if ( version_compare( $GLOBALS['wp_version'], SUBSCRIBE_NOW_MIN_WP_VERSION, '<' ) ) {
-      load_plugin_textdomain( 'subscribenow' );
-
-      $message = '<strong>'.sprintf( 'Subscribe now %s requires WordPress %s or higher.', SUBSCRIBE_NOW_VERSION, SUBSCRIBE_NOW_MIN_WP_VERSION ).'</strong> '.sprintf('Please <a href="%1$s">upgrade WordPress</a> to a current version.', 'https://codex.wordpress.org/Upgrading_WordPress', 'https://wordpress.org/extend/plugins/akismet/download/');
-
-      SubsribeNow::bail_on_activation( $message );
-    }
-  } // END public static function activate
-
-
-  public static function deactivate()
-  {
-    // Do nothing
-  } // END public static function deactivate
+        // Do nothing
+    } // END public static function deactivate
 }
 }
 
 function ajax_subscribe_init(){
-  wp_register_script('subscribenow-ajax', SUBSCRIBE_NOW_PLUGIN_URL . 'assets/subscribenow.js', array('jquery') );
-  wp_localize_script( 'login', 'ajax_subscribenow_object', array(
-    'ajaxurl' => admin_url( 'admin-ajax.php' ),
-    'redirecturl' => home_url(),
-    'loadingmessage' => __('Sending user info, please wait...')
-  ));
+    wp_register_script('subscribenow-ajax', SUBSCRIBE_NOW_PLUGIN_URL . 'assets/subscribenow.js', array('jquery') );
+    wp_localize_script( 'subscribenow-ajax', 'ajax_subscribenow_object', array(
+        'ajaxurl' => admin_url( 'admin-ajax.php' ),
+        'redirecturl' => home_url(),
+        'loadingmessage' => __('We are now sending confirmation link to your email, please wait...')
+    ));
+
+    wp_register_script('subscribenow-verify-ajax', SUBSCRIBE_NOW_PLUGIN_URL . 'assets/subscribenow-verify.js', array('jquery') );
+    wp_localize_script( 'subscribenow-verify-ajax', 'ajax_subscribenow_object', array(
+        'ajaxurl' => admin_url( 'admin-ajax.php' ),
+        'redirecturl' => home_url(),
+        'loadingmessage' => __('Completing your subscription, please wait...')
+    ));
+
+    add_action( 'wp_ajax_nopriv_ajax_subscribe_save', 'ajax_subscribe_save' );
+    add_action( 'wp_ajax_nopriv_ajax_subscribe_verify', 'ajax_subscribe_verify' );
 }
+
 function ajax_subscribe_save(){
 
-  // First check the nonce, if it fails the function will break
-  check_ajax_referer( 'ajax-subscription-nonce', 'security' );
+    // First check the nonce, if it fails the function will break
+    check_ajax_referer( 'ajax-subscription-nonce', 'security' );
 
-  // Nonce is checked, get the POST data and sign user on
-  $info = array();
-  $info['email'] = $_POST['email'];
+    // Nonce is checked, get the POST data and sign user on
+    $info = array();
+    $info['email'] = $_POST['email'];
+    $newmember = addNewMember($info['email']);
+    if ($newmember === 'exist'){
+        echo json_encode(array('sent'=>'exist', 'message'=>__('Email already on mailing list. Please check your email')));
+    } else if ( is_wp_error($newmember) ){
+        echo json_encode(array('sent'=>false, 'message'=>__('Something went wrong.')));
+    } else {
+        // Send Email
+        echo json_encode(array('sent'=>true, 'message'=>__('Email confirmation sent. Please check your email')));
+    }
 
-  if ( is_wp_error() ){
-    echo json_encode(array('loggedin'=>false, 'message'=>__('Wrong username or password.')));
-  } else {
-    echo json_encode(array('loggedin'=>true, 'message'=>__('Login successful, redirecting...')));
-  }
+    die();
+}
 
-  die();
+function ajax_subscribe_verify(){
+
+    // First check the nonce, if it fails the function will break
+    check_ajax_referer( 'ajax-subscription-nonce', 'security' );
+
+    // Nonce is checked, get the POST data and sign user on
+    $info = array();
+    $info['email'] = trim($_POST['email']);
+    $info['fullname'] = ucwords(strtolower(trim($_POST['fullname'])));
+    $info['nickname'] = strtolower(trim($_POST['fullname']));
+    $info['contact'] = trim($_POST['contact']);
+    $newmember = addNewMemberInfo($info['email'],$info['fullname'],$info['nickname'],$info['contact']);
+    if ($newmember === 'exist'){
+        echo json_encode(array('sent'=>'notfound', 'message'=>__('Email not found on mailing list.')));
+    } else if ( is_wp_error($newmember) ){
+        echo json_encode(array('sent'=>false, 'message'=>__('Something went wrong.')));
+    } else {
+        // Add to Mailing List
+
+        // Send Email
+
+        echo json_encode(array('sent'=>true, 'message'=>__('Subscription Complete. You will now start receiving newsletters')));
+    }
+
+    die();
+}
+
+function addNewMember($email){
+    require_once(SUBSCRIBE_NOW_PLUGIN_DIR . 'lib/class-member.php');
+    $member = new Member();
+    return $member->addMemberToList($email);
+}
+
+function addNewMemberInfo($email,$fullname,$nickname,$contact){
+    require_once(SUBSCRIBE_NOW_PLUGIN_DIR . 'lib/class-member.php');
+    $member = new Member();
+    return $member->addMemberInfo($email,$fullname,$nickname,$contact);
 }
